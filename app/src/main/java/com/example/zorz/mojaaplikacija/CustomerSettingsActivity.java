fd7f6cd97e371cbf9e8e00822d2ab912c3bb2621
milicaVisnjic <milicavishnjich@gmail.com>
2018-02-02 23:16:57 +0100

    jhj
    jj

--- app/src/main/java/com/example/zorz/mojaaplikacija/CustomerSettingsActivity.java
+++ app/src/main/java/com/example/zorz/mojaaplikacija/CustomerSettingsActivity.java
@@ -1,21 +1,40 @@
 package com.example.zorz.mojaaplikacija;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static android.provider.MediaStore.*;
 
 public class CustomerSettingsActivity extends AppCompatActivity {
 
@@ -29,6 +48,11 @@ public class CustomerSettingsActivity extends AppCompatActivity {
     private String userID;
     private String mName;
     private String mPhone;
+    private String mProfileImageUrl;
+
+    private ImageView mProfileImage;
+
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +61,9 @@ public class CustomerSettingsActivity extends AppCompatActivity {
 
         mNameField = (EditText) findViewById(R.id.name);
         mPhoneField = (EditText) findViewById(R.id.phone);
+
+        mProfileImage = (ImageView) findViewById(R.id.profileImage);
+
         mBack = (Button) findViewById(R.id.back);
         mConfirm = (Button) findViewById(R.id.confirm);
 
@@ -46,10 +73,23 @@ public class CustomerSettingsActivity extends AppCompatActivity {
 
         getUserInfo();
 
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1); //broj 1 je broj od bas ovog intenta, tj za biranje slike iz galerije
+            }
+        });
+
         mConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveUserInformation();
+                try {
+                    saveUserInformation();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
         mBack.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +115,10 @@ public class CustomerSettingsActivity extends AppCompatActivity {
                         mPhone = map.get("phone").toString();
                         mPhoneField.setText(mPhone);
                     }
+                    if(map.get("profileImageUrl") != null) {
+                        mProfileImageUrl = map.get("profileImageUrl").toString();
+                        Glide.with(getApplication()).load(mProfileImageUrl).into(mProfileImage);
+                    }
                 }
             }
 
@@ -85,7 +129,7 @@ public class CustomerSettingsActivity extends AppCompatActivity {
         });
     }
 
-    private void saveUserInformation() {
+    private void saveUserInformation() throws IOException {
         mName = mNameField.getText().toString();
         mPhone = mPhoneField.getText().toString();
 
@@ -94,6 +138,54 @@ public class CustomerSettingsActivity extends AppCompatActivity {
         userInfo.put("phone", mPhone);
         mCustomerDatabase.updateChildren(userInfo);
 
-        finish();
+        if(resultUri != null) { //ako je korisnik izabrao sliku
+            StorageReference filePath = FirebaseStorage.getInstance().getReference().child("profile_images").child(userID);
+            Bitmap bitmap = null;
+            //pretvaramo Uri u sliku
+
+            bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(); //kompresujemo sliku
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = filePath.putBytes(data); //uploadujemo sliku u Storage
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    finish();
+                    return;
+                }
+            });
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    Map newImage = new HashMap();
+                    newImage.put("profileImageUrl", downloadUrl.toString());
+                    mCustomerDatabase.updateChildren(newImage);
+
+                    finish();
+                    return;
+                }
+            });
+        }
+        else {
+            finish();
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            final Uri imageUri = data.getData(); //ovaj Uri ima tacnu lokaciju slike koju je korisnik izabrao
+            resultUri = imageUri;
+            mProfileImage .setImageURI(resultUri); //postavljamo imageView u sliku koju smo dobili kada je korisnik izabrao sliku iz galerije
+
+        }
     }
 }
